48792de5c0b051dea171164de6fbb1d2268d8707
haisen <49050590+karenzhangxd@users.noreply.github.com>
2020-12-12 20:57:55 +0800

    修改request接口地址

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export function request(config) {
   const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://81.68.227.246:3000',
     // timeout: 5000
   })
   //添加请求拦截器添加token
